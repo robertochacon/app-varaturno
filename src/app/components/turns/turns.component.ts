353a7346f51cb9ae67ab7db371c1f33e05f88ae5
rchaconalcantara <hola.hack09@gmail.com>
2023-03-10 19:50:51 -0400

    improvements

--- src/app/components/turns/turns.component.ts
+++ src/app/components/turns/turns.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from 'src/app/services/services.service';
 
 @Component({
   selector: 'app-turns',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TurnsComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  loadData = false;
+  result = '';
+  name = '';
+  description = '';
+  listServices: any[] = [];
+
+  constructor(private _entities: ServicesService) { }
 
   ngOnInit(): void {
+    this.getAllEntities();
+  }
+
+  getAllEntities(){
+    this.loading = true;
+
+    this._entities.getAllServices().subscribe((response)=>{
+
+      this.listServices = response.data;
+
+      setTimeout(function(){
+        console.log(response.data);
+        
+      },100);
+      this.loading = false;
+      
+    }, error=>{
+        this.loadData = false;
+        this.loading = false;
+    })
+
   }
 
 }
